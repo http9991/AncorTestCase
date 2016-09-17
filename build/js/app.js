3bfaa27db5ed13646a23ad1f748177627c93bb14
Kadrov Maxim <kadrovmax@gmail.com>
2016-09-17 22:25:48 +0300

    fix skidka-2

--- build/js/app.js
+++ build/js/app.js
@@ -1,37 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function(){
   var priceArr = [];
-  $('.addButton').on('click', function() {
+  $('.addButton').on('click', function(){
     var productName = $('#productName').val().trim();
-    var productPrice = Math.floor(+($('#productPrice').val()) * 100) / 100;
+    var productPrice = Math.floor(+($('#productPrice').val())*100)/100;
     $('#productName').val('');
     $('#productPrice').val('');
-    if (productName !== '' && isNumeric(productPrice) && productPrice !== 0) {
+    if(productName !== '' && isNumeric(productPrice) && productPrice !== 0){
       addProduct(productName, productPrice);
-    } else {
+    }
+    else{
       alert('Неправильный формат добавления товара!');
     }
   });
-  $('.applyButton').on('click', function() {
+  $('.applyButton').on('click', function(){
     var off = $('#off').val();
-    if (isNumeric(off) && off !== 0 && (off % 1 === 0)) {
+    if(isNumeric(off) && off !== 0 && (off % 1 === 0)){
       calculateOff(off);
-    } else {
+    }
+    else{
       alert('Неправильный формат скидки!');
     }
   });
-
-  function isNumeric(n) {
+  function isNumeric(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
-
-  function addProduct(productName, productPrice) {
+  function addProduct(productName, productPrice){
     $('.productTable > tbody:last-child').append('<tr><td>' + productName + '</td><td class="fullPrice">' + productPrice + '</td><td>' + '?????' + '</td></tr>');
     priceArr.push(productPrice);
     var off = $('#off').val();
     calculateOff(off);
   }
-
-  function calculateOff(off) {
+  function calculateOff(off){
     var totalPrice = priceArr.reduce(function(sum, current) {
       return +(sum) + (+current);
     });
@@ -43,19 +42,22 @@ $(document).ready(function() {
     var biggestPrice = {
       price: 0,
     };
-    $('.fullPrice').each(function() {
+    $('.fullPrice').each(function(){
       var currentPrice = +(this.innerHTML);
-      if (biggestPrice.price <= currentPrice) {
+      if(biggestPrice.price <= currentPrice){
         biggestPrice.price = currentPrice;
         biggestPrice.element = this;
       }
       var currentSale = (currentPrice / totalPrice) * off;
-      dynamicSale += currentSale - Math.round(currentSale * 100) / 100;
-      this.nextSibling.innerHTML = currentPrice - Math.round(currentSale * 100) / 100;
-    });
+        dynamicSale += currentSale - Math.round(currentSale * 100)/100;
+        this.nextSibling.innerHTML = (currentPrice - Math.round(currentSale * 100)/100).toFixed(2);
+        
 
-    if (+(dynamicSale) !== 0) {
-      biggestPrice.element.nextSibling.innerHTML = (+(biggestPrice.element.nextSibling.innerHTML) - (Math.round(dynamicSale * 100) / 100)).toFixed(2);
+    });
+    
+    if(+(dynamicSale) !== 0){
+      console.log(+(biggestPrice.element.nextSibling.innerHTML) - (Math.round(dynamicSale*100)/100));
+      biggestPrice.element.nextSibling.innerHTML = (+(biggestPrice.element.nextSibling.innerHTML) - (Math.round(dynamicSale*100)/100)).toFixed(2);
     }
   }
 });
