63793c1201b75e4b1ba93f2cce7e3aacb77adf7a
Kadrov Maxim <kadrovmax@gmail.com>
2016-09-19 03:15:58 +0300

    result

--- build/js/app.js
+++ build/js/app.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
   var priceArr = [];
   $('.addButton').on('click', function(){
     var productName = $('#productName').val().trim();
-    var productPrice = Math.floor(+($('#productPrice').val())*100)/100;
+    var productPrice = +($('#productPrice').val());
+    if(+(Math.floor(productPrice * 100) / 100).toFixed(2) !== +productPrice){
+      alert('Неправильный формат цены!');
+      return;
+    }
     $('#productName').val('');
     $('#productPrice').val('');
     if(productName !== '' && isNumeric(productPrice) && productPrice !== 0){
@@ -14,7 +18,7 @@ $(document).ready(function(){
   });
   $('.applyButton').on('click', function(){
     var off = $('#off').val();
-    if(isNumeric(off) && off !== 0 && (off % 1 === 0)){
+    if(off !== 0 && isNumeric(off) && off > 0 && (off % 1 === 0)){
       calculateOff(off);
     }
     else{
@@ -25,15 +29,27 @@ $(document).ready(function(){
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
   function addProduct(productName, productPrice){
-    $('.productTable > tbody:last-child').append('<tr><td>' + productName + '</td><td class="fullPrice">' + productPrice + '</td><td>' + '?????' + '</td></tr>');
+    if(productPrice < 0){
+      alert('Цена меньше нуля!');
+      return;
+    }
+    if(productPrice === Math.floor(productPrice)){
+      productPrice = Math.floor(productPrice);
+    }
+    $('.productTable > tbody:last-child').append('<tr><td>' + productName + '</td><td class="fullPrice">' + productPrice + '</td><td></td></tr>');
     priceArr.push(productPrice);
     var off = $('#off').val();
     calculateOff(off);
   }
   function calculateOff(off){
+    if(!priceArr[0]){
+      alert('Нечего считать! нет товаров в корзине');
+      return;
+    }
     var totalPrice = priceArr.reduce(function(sum, current) {
-      return +(sum) + (+current);
+      return sum + current;
     });
+
     if (totalPrice < off) {
       alert('Скидка больше цены!');
       return;
@@ -49,14 +65,14 @@ $(document).ready(function(){
         biggestPrice.element = this;
       }
       var currentSale = (currentPrice / totalPrice) * off;
-        dynamicSale += currentSale - Math.round(currentSale * 100)/100;
-        this.nextSibling.innerHTML = (currentPrice - Math.round(currentSale * 100)/100).toFixed(2);
-        
-
+      var currentSaleTwoPoints = +(Math.floor(currentSale * 100) / 100).toFixed(2);
+        dynamicSale += currentSale - currentSaleTwoPoints;
+        var result = currentPrice - currentSaleTwoPoints;
+        this.nextSibling.innerHTML = +(Math.floor(result * 100) / 100).toFixed(2);
     });
-    
-    if(+(dynamicSale) !== 0){
-      biggestPrice.element.nextSibling.innerHTML = (+(biggestPrice.element.nextSibling.innerHTML) - (Math.round(dynamicSale*100)/100)).toFixed(2);
+    if(dynamicSale !== 0){
+      var saleRemains = +(biggestPrice.element.nextSibling.innerHTML) - (+(dynamicSale).toFixed(2));
+      biggestPrice.element.nextSibling.innerHTML = +(Math.floor(saleRemains * 100) / 100).toFixed(2);
     }
   }
 });
